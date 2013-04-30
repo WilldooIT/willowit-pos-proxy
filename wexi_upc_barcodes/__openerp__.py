9036d8d343666e67bd609708bef57e7ea91a10da
Thomas Cook <thomas@willowit.com.au>
2013-04-30 14:59:55 +1000

    Module that adds support for upc barcodes

---
+++ wexi_upc_barcodes/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "WEXI - Enable support for upc barcodes",
+    "version": "1.0",
+    "depends": [
+                "product"
+                ],
+    "author": "Thomas Cook - Willow IT",
+    "category": "Wexi",
+    "description": """Enable the use of UPC barcodes on products""",
+    'data': [
+             ],
+    "demo": [],
+    "test": [],
+    "installable": True,
+    "active": False,
+}
