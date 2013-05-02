ddf0f45624fb12758a220b70dad6ab51f1c1afe4
Thomas Cook <thomas@willowit.com.au>
2013-05-02 10:35:37 +1000

    Minor change to startup behavior

--- pos_proxy/pre_launch.py
+++ pos_proxy/pre_launch.py
@@ -6,12 +6,18 @@ import requests
 configpath = os.environ["POS_PROXY_PATH"] + "/config.json"
 config = json.load(open(configpath))
 for (p_name,printer) in config["printers"].iteritems():
-	if printer["type"] == "local":
-	    mode = os.stat(printer["device"]).st_mode | stat.S_IWOTH
-	    os.chmod(printer["device"],mode)
-	if config.get("vfd_device"):
-	    mode = os.stat(config["vfd_device"]).st_mode | stat.S_IWOTH
-	    os.chmod(config["vfd_device"],mode)
+    try:
+        if printer["type"] == "local":
+            mode = os.stat(printer["device"]).st_mode | stat.S_IWOTH
+            os.chmod(printer["device"],mode)
+    except:
+        print "Printer device not found!"
+    try:
+        if config.get("vfd_device"):
+            mode = os.stat(config["vfd_device"]).st_mode | stat.S_IWOTH
+            os.chmod(config["vfd_device"],mode)
+    except:
+        print "VFD not found!"
 
 #try:
 #    response = requests.get(config["cookbook_url"],verify=False)
